5c5997c5f8d869ed23895d3344305f67a79cce23
shwetachavda <shwetachavda13@gmail.com>
2016-02-04 23:50:30 -0500

    added contact and aboutme pages

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
+/*GET about page */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+/*GET contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
 
 module.exports = router;
